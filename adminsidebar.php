06819ef65b9e9d371ea719211ef6d3f192dccdd8
Oke Abisola <bisolaoe@gmail.com>
2024-01-11 12:38:11 +0100

    first commit

---
+++ adminsidebar.php
@@ -0,0 +1,67 @@
+<aside class="main-sidebar">
+            <!-- sidebar -->
+            <div class="sidebar">
+               <!-- sidebar menu -->
+               <ul class="sidebar-menu">
+                  <li class="active">
+                     <a href="adminhome.php"><i class="fa fa-tachometer"></i><span>Dashboard</span>
+                     <span class="pull-right-container">
+                     </span>
+                     </a>
+                  </li>
+                   <li class="treeview">
+                     <a href="#">
+                     <i class="fa fa-users"></i><span>Settings</span>
+                     <span class="pull-right-container">
+                     <i class="fa fa-angle-left pull-right"></i>
+                     </span>
+                     </a>
+                     <ul class="treeview-menu">
+                        <li><a href="course.php">Add Courses</a></li>
+                        <li><a href="addolevel.php">Add Subjects</a></li>
+                        <li><a href="addgrade.php">Add Grades</a></li>
+                        <li><a href="addyear.php">Add Year</a></li>
+                        <li><a href="changepwdadmin.php">Change Password</a></li>     
+                        <li><a href="logoutadmin.php">Logout</a></li>
+                        
+                     </ul>
+                  </li>
+                  
+
+                  <li class="treeview">
+                     <a href="#">
+                     <i class="fa fa-users"></i><span>Application</span>
+                     <span class="pull-right-container">
+                     <i class="fa fa-angle-left pull-right"></i>
+                     </span>
+                     </a>
+                     <ul class="treeview-menu">
+                        <li><a href="applicant.php">Applicant List</a></li>
+                        <li><a href="admission.php">Admission List</a></li>
+                        <li><a href="editapplicant.php">Edit Applicant Biodata</a></li>
+                        <li><a href="peyapp.php">Application Setup</a></li>   
+                        <li><a href="viewpayment.php">View App Payments</a></li>
+                     </ul>
+                  </li>
+                                    
+                  
+                  <li>
+                     <a href="adminhome.php">
+                     <i class="fa fa-home"></i> <span>Support Applicants</span>
+                     <span class="pull-right-container">
+                     </span>
+                     </a>
+                  </li>
+                  <li>
+                     <a href="adminhome.php">
+                     <i class="fa fa-stop-circle"></i> <span>View logs</span>
+                     <span class="pull-right-container">
+                     </span>
+                  </li>
+                 
+                     </a>
+                  </li>
+               </ul>
+            </div>
+            <!-- /.sidebar -->
+         </aside>
